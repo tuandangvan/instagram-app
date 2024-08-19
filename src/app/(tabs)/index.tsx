921b2e987bcc81ab75aa8db79974fbc01cc7c290
VanTuan <tuandangvann@gmail.com>
2024-08-20 02:23:01 +0700

    responsive

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function FeedScreen() {
     return (
         <View className="bg-slate-600 flex-1 items-center justify-center m-10">
-            <Text className="text-5xl font-bold text-blue-500">Feed</Text>
+            <Text className="text-md md:text-5xl font-bold text-blue-500 md:text-red-500">Feed</Text>
         </View>
     )
 }
